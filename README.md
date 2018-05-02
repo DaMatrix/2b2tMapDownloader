78404113bfba8d627956ace1cd395a643bb3a34a
DaMatrix <joey.rabil@gmail.com>
2018-05-02 16:54:47 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# The 2b2t Map Downloader
+
+*A quest to bring 2b2t's map to the public*
+
+*i.e. steal it from hause*
+
+*jeff*
+
+(((This readme is WIP, nothing to see here)))
---
+++ client/src/main/java/net/daporkchop/mapdl/client/Client.java
@@ -0,0 +1,128 @@
+/*
+ * Adapted from the Wizardry License
+ *
+ * Copyright (c) 2018-2018 DaPorkchop_ and contributors
+ *
+ * Permission is hereby granted to any persons and/or organizations using this software to copy, modify, merge, publish, and distribute it. Said persons and/or organizations are not allowed to use the software or any derivatives of the work for commercial use or any other means to generate income, nor are they allowed to claim this software as their own.
+ *
+ * The persons and/or organizations are also disallowed from sub-licensing and/or trademarking this software without explicit permission from DaPorkchop_.
+ *
+ * Any persons and/or organizations using this software must disclose their source code and have it publicly available, include this license, provide sufficient credit to the original authors of the project (IE: DaPorkchop_), as well as provide a link to the original project.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package net.daporkchop.mapdl.client;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.event.RegistryEvent;
+
+@Mod(
+        modid = Client.MOD_ID,
+        name = Client.MOD_NAME,
+        version = Client.VERSION
+)
+public class Client {
+
+    public static final String MOD_ID = "client";
+    public static final String MOD_NAME = "Client";
+    public static final String VERSION = "0.0.1";
+
+    /**
+     * This is the instance of your mod as created by Forge. It will never be null.
+     */
+    @Mod.Instance(MOD_ID)
+    public static Client INSTANCE;
+
+    /**
+     * This is the first initialization event. Register tile entities here.
+     * The registry events below will have fired prior to entry to this method.
+     */
+    @Mod.EventHandler
+    public void preinit(FMLPreInitializationEvent event) {
+
+    }
+
+    /**
+     * This is the second initialization event. Register custom recipes
+     */
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    /**
+     * This is the final initialization event. Register actions from other mods here
+     */
+    @Mod.EventHandler
+    public void postinit(FMLPostInitializationEvent event) {
+
+    }
+
+    /**
+     * Forge will automatically look up and bind blocks to the fields in this class
+     * based on their registry name.
+     */
+    @GameRegistry.ObjectHolder(MOD_ID)
+    public static class Blocks {
+      /*
+          public static final MySpecialBlock mySpecialBlock = null; // placeholder for special block below
+      */
+    }
+
+    /**
+     * Forge will automatically look up and bind items to the fields in this class
+     * based on their registry name.
+     */
+    @GameRegistry.ObjectHolder(MOD_ID)
+    public static class Items {
+      /*
+          public static final ItemBlock mySpecialBlock = null; // itemblock for the block above
+          public static final MySpecialItem mySpecialItem = null; // placeholder for special item below
+      */
+    }
+
+    /**
+     * This is a special class that listens to registry events, to allow creation of mod blocks and items at the proper time.
+     */
+    @Mod.EventBusSubscriber
+    public static class ObjectRegistryHandler {
+        /**
+         * Listen for the register event for creating custom items
+         */
+        @SubscribeEvent
+        public static void addItems(RegistryEvent.Register<Item> event) {
+           /*
+             event.getRegistry().register(new ItemBlock(Blocks.myBlock).setRegistryName(MOD_ID, "myBlock"));
+             event.getRegistry().register(new MySpecialItem().setRegistryName(MOD_ID, "mySpecialItem"));
+            */
+        }
+
+        /**
+         * Listen for the register event for creating custom blocks
+         */
+        @SubscribeEvent
+        public static void addBlocks(RegistryEvent.Register<Block> event) {
+           /*
+             event.getRegistry().register(new MySpecialBlock().setRegistryName(MOD_ID, "mySpecialBlock"));
+            */
+        }
+    }
+    /* EXAMPLE ITEM AND BLOCK - you probably want these in separate files
+    public static class MySpecialItem extends Item {
+
+    }
+
+    public static class MySpecialBlock extends Block {
+
+    }
+    */
+}
