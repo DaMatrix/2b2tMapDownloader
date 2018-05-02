a168f43a8586d5ea1de2ca0ba23e1aac65f18c0d
DaMatrix <joey.rabil@gmail.com>
2018-05-02 18:06:35 +0200

    start working on config

--- server/src/main/java/net/daporkchop/mapdl/server/ServerMain.java
+++ server/src/main/java/net/daporkchop/mapdl/server/ServerMain.java
@@ -13,40 +13,13 @@
  *
  */
 
-package net.daporkchop.mapdl.client;
+package net.daporkchop.mapdl.server;
 
 import net.daporkchop.mapdl.common.MapSession;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(
-        modid = Client.MOD_ID,
-        name = Client.MOD_NAME,
-        version = Client.VERSION,
-        dependencies = "required-after:depmanager@[0.0.1,);"
-)
-public class Client {
-    public static final String MOD_ID = "client";
-    public static final String MOD_NAME = "Client";
-    public static final String VERSION = "0.0.1";
-
-    @Mod.Instance(MOD_ID)
-    public static Client INSTANCE;
-
-    @Mod.EventHandler
-    public void preinit(FMLPreInitializationEvent event) {
-
-    }
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-
-    }
-
-    @Mod.EventHandler
-    public void postinit(FMLPostInitializationEvent event) {
-
-    }
+/**
+ * @author DaPorkchop_
+ */
+public class ServerMain {
+    public MapSession session;
 }
